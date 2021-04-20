48f0bee65f40230be7c9fed7ad8ab890ce720771
Albert Moravec <albert.moravec@gmail.com>
2021-04-21 00:21:11 +0200

    Construct priv path properly

--- bidding_poc/lib/bidding_poc_web/controllers/page_controller.ex
+++ bidding_poc/lib/bidding_poc_web/controllers/page_controller.ex
@@ -2,6 +2,11 @@ defmodule BiddingPocWeb.PageController do
   use BiddingPocWeb, :controller
 
   def index(conn, _params) do
-    html(conn, File.read!("priv/static/index.html"))
+    file =
+      :code.priv_dir(:bidding_poc)
+      |> Path.join("static/index.html")
+      |> File.read!()
+
+    html(conn, file)
   end
 end
