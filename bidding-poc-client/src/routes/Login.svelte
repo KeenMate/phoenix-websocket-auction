27b143199d8bfe8a35995e220804e11d8c002895
Albert Moravec <albert.moravec@gmail.com>
2021-04-21 01:33:15 +0200

    Beautiful login name table

--- bidding-poc-client/src/routes/Login.svelte
+++ bidding-poc-client/src/routes/Login.svelte
@@ -1,55 +1,62 @@
 <script>
-	import {push, querystring} from "svelte-spa-router"
-	import {parse} from "qs"
-	import toastr from "../helpers/toastr-helpers"
-	import {storeAuthToken, loginUser, authTokenStore, userStore} from "../providers/auth"
-	import {UnauthorizedError} from "../providers/exceptions"
-	import LoginForm from "../components/LoginForm.svelte"
+  import { push, querystring } from "svelte-spa-router";
+  import { parse } from "qs";
+  import toastr from "../helpers/toastr-helpers";
+  import { storeAuthToken, loginUser, authTokenStore, userStore } from "../providers/auth";
+  import { UnauthorizedError } from "../providers/exceptions";
+  import LoginForm from "../components/LoginForm.svelte";
 
-	$: query = parse($querystring)
+  $: query = parse($querystring);
 
-	let username = ""
-	let password = ""
+  let username = "";
+  let password = "";
 
-	async function onLogin() {
-		if (!username || !password) {
-			toastr.warning("Username and password must be filled in")
-			return
-		}
+  async function onLogin() {
+    if (!username || !password) {
+      toastr.warning("Username and password must be filled in");
+      return;
+    }
 
-		try {
-			const {user, token} = await loginUser(username, password)
-			storeAuthToken(token)
-			authTokenStore.set(token)
-			userStore.set(user)
+    try {
+      const { user, token } = await loginUser(username, password);
+      storeAuthToken(token);
+      authTokenStore.set(token);
+      userStore.set(user);
 
-			push(query.redirect || "/")
-		} catch (err) {
-			if (err instanceof UnauthorizedError)
-				toastr.error("Incorrect credentials")
-			else {
-				console.error("Error occured while logging in user", err)
-				toastr.error("Could not authenticate user")
-			}
+      push(query.redirect || "/");
+    } catch (err) {
+      if (err instanceof UnauthorizedError) toastr.error("Incorrect credentials");
+      else {
+        console.error("Error occured while logging in user", err);
+        toastr.error("Could not authenticate user");
+      }
 
-			throw err
-		}
-	}
+      throw err;
+    }
+  }
 </script>
 
 <div class="login-page">
-	<div class="columns is-centered">
-		<div class="column is-4">
-			<h4 class="is-text-4">
-				Login
-			</h4>
-			<LoginForm
-				{username}
-				{password}
-				on:changeUsername={ev => username = ev.detail}
-				on:changePassword={ev => password = ev.detail}
-				on:login={onLogin}
-			/>
-		</div>
-	</div>
+  <div class="columns is-centered">
+    <div class="column is-4">
+      <h4 class="is-text-4">Login</h4>
+      <LoginForm
+        {username}
+        {password}
+        on:changeUsername={(ev) => (username = ev.detail)}
+        on:changePassword={(ev) => (password = ev.detail)}
+        on:login={onLogin}
+      />
+      <p>
+        <b>Login names:</b><br />
+        Richmond, Nash <br />
+        Barron, Zephania <br />
+        Banks, Hyatt <br />
+        Murray, Christen <br />
+        Adams, Xerxes <br />
+        Oliver, Geoffrey <br />
+        <b>Password: 1234</b>
+      </p>
+    </div>
+  </div>
 </div>
