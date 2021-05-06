5c6bdb4a54ac402956839d685b8b3123821bfc64
Filip Jakab <jakabfilip.fj@gmail.com>
2021-05-07 00:20:40 +0200

    Fixed WS url to not to point to localhost

--- bidding-poc-client/src/constants/urls.js
+++ bidding-poc-client/src/constants/urls.js
@@ -2,4 +2,4 @@ const Production = process.env.SVELTE_APP_PRODUCTION === "true"
 const ServerHost = new URL(process.env.SVELTE_APP_SERVER_HOSTNAME)
 
 export const ApiUrl = Production ? "/api" : `${ServerHost.toString()}api`
-export const SocketUrl = `ws${Production && "s" || ""}://${ServerHost.host}/socket`
+export const SocketUrl = Production ? "/socket" : `ws://${ServerHost.host}/socket`
